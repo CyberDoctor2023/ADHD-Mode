5cf29c2914e3b7dcd41fb083b1ae7d9203a7d3b9
jack <ilovecyberhk@gmail.com>
2025-08-19 13:10:15 +0800

    Initial Commit

---
+++ ADHDMode/ADHDModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADHDModeApp.swift
+//  ADHDMode
+//
+//  Created by jack on 8/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ADHDModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ADHDModeTests/ADHDModeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ADHDModeTests.swift
+//  ADHDModeTests
+//
+//  Created by jack on 8/19/25.
+//
+
+import Testing
+@testable import ADHDMode
+
+struct ADHDModeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
